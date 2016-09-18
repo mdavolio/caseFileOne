010ef3c4fa1da570fffe51e6395c250bbaa1bcab
Matt <md3es@virginia.edu>
2016-09-18 11:52:18 -0400

    street cam plot attempt

--- case_study_1.R
+++ case_study_1.R
@@ -6,6 +6,7 @@ library(MASS)
 library(dplyr)
 library(rgdal)
 
+
 crime2016 <- read_csv('DataSets/Crime_Incidents__2016.csv')
 crime2015 <- read_csv('DataSets/Crime_Incidents__2015.csv')
 crime2014 <- read_csv('DataSets/Crime_Incidents__2014.csv')
@@ -29,10 +30,21 @@ plot(dcBorder)
 palette.function = colorRampPalette(rev(brewer.pal(11,'Spectral')))
 heat.colors = palette.function(32)
 
-# Crime Locations
+# Crime Locations 2016
 names(crime2016)[1]<-paste("X")
 crime2016Loc <- as.data.frame(subset(crime2016, select = c('X','Y')))
 
 # Plot Crime KDE
 est = kde2d(crime2016Loc[,'X'], crime2016Loc[,'Y'], h=0.1, n=c(100,100))  # h=0.1
 image(est, col = heat.colors, useRaster=TRUE, asp=1)
+
+# Plot Street Cam locations
+colnames(streetCams)[colnames(streetCams) == 'LONGITUDE'] <- 'Long'
+colnames(streetCams)[colnames(streetCams) == 'LATITUDE'] <- 'Lat'
+streetCams[, c('Long', 'Lat')] <- sapply(streetCams[, c('Long', 'Lat')], as.factor)
+
+streetCamsLocation <- as.data.frame(subset(streetCams, select = c(('Long'), ('Lat')))) %>%
+  filter('Lat' > 0) # NEED TO REMOVE ZEROS?????
+plot(streetCamsLocation)
+                                    
+                                    
