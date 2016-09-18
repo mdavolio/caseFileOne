5fa16b506d3c9bd255abfa525c5c3386f9f403c4
Matt <md3es@virginia.edu>
2016-09-18 10:15:28 -0400

    initial kde plotting

--- case_study_1.R
+++ case_study_1.R
@@ -1,7 +1,9 @@
 library(readr)
-library(dplyr)
 library(readxl)
 library(maptools)
+library(RColorBrewer)
+library(MASS)
+library(dplyr)
 
 crime2016 <- read_csv('DataSets/Crime_Incidents__2016.csv')
 crime2015 <- read_csv('DataSets/Crime_Incidents__2015.csv')
@@ -15,5 +17,15 @@ wardDemo <- read_csv('DataSets/wardDemographics.csv')
 wardWellBeing <- read_csv('DataSets/wardWellBeing.csv')
 metroBus <- read_csv('DataSets/Metro_Bus_Stops.csv')
 metroStations <- read_csv('DataSets/Metro_Stations_Regional.csv')
-dcBorder <- readShapeSpatial('DataSets/district_of_columbia_administrative.shp')
-plot(dcBorder)
+
+# get color palette for heat map
+palette.function = colorRampPalette(rev(brewer.pal(11,'Spectral')))
+heat.colors = palette.function(32)
+
+# Crime Locations
+names(crime2016)[1]<-paste("X")
+crime2016Loc <- as.data.frame(subset(crime2016, select = c('X','Y')))
+
+# Plot Crime KDE
+est = kde2d(crime2016Loc[,'X'], crime2016Loc[,'Y'], h=0.1, n=c(100,100))  # h=0.1
+image(est, col = heat.colors, useRaster=TRUE, asp=1)
